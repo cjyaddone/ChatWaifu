4af7016066e9ead0083da509a16565ee18942706
cjyaddone <54592829+cjyaddone@users.noreply.github.com>
2022-12-17 18:58:52 -0800

    Update mandarin.py

--- text/mandarin.py
+++ text/mandarin.py
@@ -7,7 +7,7 @@ import cn2an
 import logging
 
 logging.getLogger('jieba').setLevel(logging.WARNING)
-jieba.set_dictionary(os.path.dirname(sys.argv[0])+'/jieba/dict.txt')
+jieba.set_dictionary(r'./jieba/dict.txt')
 jieba.initialize()
 
 
